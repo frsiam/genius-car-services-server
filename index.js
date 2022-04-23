ee9f6b47d0a566b1d1dcab4bba301a651de33298
frsiam <siam.frs@gmail.com>
2022-04-23 14:26:51 +0600

    create environment variabel for secure the password

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors');
 const port = process.env.PORT || 4000;
 require('dotenv').config();
@@ -7,6 +8,17 @@ const app = express();
 // midlleware 
 app.use(cors());
 app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:<password>@cluster0.mdofc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    console.log('Genius car database connected')
+    // perform actions on the collection object
+    client.close();
+});
+
 
 app.get('/', (req, res) => {
     res.send('Running genius server');
